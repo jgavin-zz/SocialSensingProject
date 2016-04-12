fdf258ee5338ab3dc84b76e964f07b1e92e2131d
jgavin <jgavin@nd.edu>
2016-04-11 23:23:52 -0400

    working on keywords

---
+++ keywords_test.py
@@ -0,0 +1,8 @@
+from check_basketball_keywords import check_basketball_keywords
+
+
+s1 = "hi hello basketball words"
+s2 = "poop great"
+
+print check_basketball_keywords(s1)
+print check_basketball_keywords(s2)
