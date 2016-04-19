23f88646ffe11c104fdb3b071ad2f8fcba532beb
jgavin <jgavin@nd.edu>
2016-04-19 01:14:39 -0400

    Working on login

--- validate_user.py
+++ validate_user.py
@@ -15,7 +15,6 @@ def validate_user(email, psw):
 	exists = 0
 	for (username) in cursor:
 		user = str(''.join(username))
-		return user + ' ' + str(email)
 		if user == str(email):
 			exists = 1
 			break
