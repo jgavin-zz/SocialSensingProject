d753c70da51d827f7726a140e5666e778348be0f
jgavin <jgavin@nd.edu>
2016-04-19 01:00:46 -0400

    Working on login

--- createDB.py
+++ createDB.py
@@ -174,8 +174,8 @@ cursor.execute(createTableQuery)
 
 #Users
 createTableQuery = ('''CREATE TABLE users (
-                                           	username VARCHAR(20) NOT NULL,
-                                                password VARCHAR(20) NOT NULL,
+                                           	username VARCHAR(100) NOT NULL,
+                                                password VARCHAR(200) NOT NULL,
                                                 PRIMARY KEY(username)
                                           );'''
                     )
