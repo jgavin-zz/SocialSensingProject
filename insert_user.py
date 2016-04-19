174f06c8fed22cadb415e9eb2c7793bc858ad847
jgavin <jgavin@nd.edu>
2016-04-19 00:41:22 -0400

    working on registration

--- insert_user.py
+++ insert_user.py
@@ -8,12 +8,23 @@ def insert_user(email, password):
                               charset='utf8',
                               use_unicode=True)
 	cursor = cnx.cursor()
+	
+	
+	#Check if user already exists
+	query = ('select username from users')
+	cursor.execute(query)
+	users = []
+	for (username) in cursor:
+		user = str(''.join(username))
+		if user == str(email):
+			return "Duplicate email. Failed to register."
+	
+	
 	query = ("INSERT IGNORE INTO users VALUES ('" + str(email) + "', '" + str(password) + "');")
 	try:
 		cursor.execute(query)
 		cnx.commit()
 		cnx.close()		
 		return 'Registered successfully'
-	except Error as e:
-		
+	except Error as e:		
 		return "mysql error: %s" % e
