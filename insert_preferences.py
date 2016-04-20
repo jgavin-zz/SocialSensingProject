ff0d530849bedb4608c74ad3116a4e16fd306aa8
jgavin <jgavin@nd.edu>
2016-04-20 14:31:02 -0400

    working on customization

--- insert_preferences.py
+++ insert_preferences.py
@@ -1,8 +1,37 @@
 import mysql.connector
 
 def insert_preferences(email, virality, time, relevance):
-
-
-	return 1
+	cnx = mysql.connector.connect(user='root', password='bob',
+                              host='127.0.0.1',
+                              database='socialsensing',
+                              charset='utf8',
+                              use_unicode=True)
+	cursor = cnx.cursor()
+	
+	#Check if user exists
+	query = ('select username from users;')
+	cursor.execute(query)
+	
+	exists = 0
+	users = ''
+	for (username) in cursor:
+		user = str(''.join(username))
+		#users = users + user + ' '
+		if user == str(email):
+			exists = 1
+			break
+		
+	if exists == 1:
+		query = ("UPDATE preferences SET virality=" + str(virality) + ", time =" +  str(time) +  ", relevance =" + str(relevance) + " where id='" + email + "';")
+		try:
+			cursor.execute(query)
+			cnx.commit()
+			cnx.close()	
+			return 1	
+		except Error as e:		
+			return 0
+			
+	else:
+		return 0
 
 	
